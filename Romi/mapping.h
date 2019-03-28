6b06ebd989c3d6ebe50460eb729299a6a144fff9
jhird <jhird>
2019-03-28 16:09:21 +0000

    Added map visited symbols, fixed map bug

---
+++ Romi/mapping.h
@@ -0,0 +1,136 @@
+#ifndef _Mapping_h
+#define _Mapping_h
+#include <EEPROM.h>
+
+const byte MAP_RESOLUTION = 25;
+const byte MAP_DEFAULT_FEATURE = '#';
+const byte MAP_EXPLORED_FEATURE = '.';
+const int MAP_X=1800;
+const int MAP_Y=1800;
+
+class Mapper
+{
+    public:
+        void resetMap();
+        void printMap();
+        void updateMapFeature(byte feature, int y, int x);
+        void updateMapFeature(byte feature, float y, float x);
+        
+        int  indexToPose(int i, int map_size, int resolution);
+        int  poseToIndex(int x, int map_size, int resolution);
+        float percent();
+    private:
+        int X_size;
+        int Y_size;
+};
+
+void Mapper::resetMap()
+{
+
+    for (int i=0;i<MAP_RESOLUTION;i++)
+    {
+        for (int j=0;j<MAP_RESOLUTION;j++)
+        {
+            int eeprom_address = (i*MAP_RESOLUTION)+j;
+            
+            if (eeprom_address > 1023)
+            {
+                Serial.println(F("Error: EEPROM Address greater than 1023"));
+            }
+            else
+            {
+                EEPROM.update(eeprom_address, MAP_DEFAULT_FEATURE );
+                
+            }
+        }
+    }
+
+}
+
+void Mapper::printMap()
+{
+
+    Serial.println("Map");
+    for (int i=0;i<MAP_RESOLUTION;i++)
+    {
+        for(int j=0;j<MAP_RESOLUTION;j++)
+        {
+            int eeprom_address = (i*MAP_RESOLUTION)+j;
+            byte value;
+            value = EEPROM.read(eeprom_address);//, value);
+            Serial.print( (char)value );
+            Serial.print(" ");
+        }
+        Serial.println("");
+    }
+  
+}
+float Mapper::percent()
+{
+   int explored_cells = 0;
+   // Serial.println("Map");
+    for (int i=0;i<MAP_RESOLUTION;i++)
+    {
+        for(int j=0;j<MAP_RESOLUTION;j++)
+        {
+            int eeprom_address = (i*MAP_RESOLUTION)+j;
+            byte value;
+            value = EEPROM.read(eeprom_address);//, value);
+            if ((char)value != MAP_DEFAULT_FEATURE)
+            {
+              explored_cells = explored_cells+1;
+            }
+        }
+        //Serial.println("");
+    }
+  float percent = float(explored_cells)/float(MAP_RESOLUTION*MAP_RESOLUTION);
+  Serial.print("percent explored: ");
+  Serial.println(percent);
+  return percent;
+}
+
+int Mapper::poseToIndex(int x, int map_size, int resolution)
+{
+    //1800/ (1800/25)
+    return x / (map_size / resolution);
+}
+
+int Mapper::indexToPose(int i, int map_size, int resolution)
+{
+    return i* (map_size / resolution);
+}
+
+
+void Mapper::updateMapFeature(byte feature, float y, float x) {
+  updateMapFeature( feature, (int)y, (int)x );  
+}
+
+void Mapper::updateMapFeature(byte feature, int y, int x)
+{
+    if (x >= MAP_X || x < 0 || y >= MAP_Y || y < 0)
+    {
+      Serial.println(F("Error:Invalid co-ordinate"));
+      return;
+    }
+
+    int x_index = poseToIndex(x, MAP_X, MAP_RESOLUTION);
+    int y_index = poseToIndex(y, MAP_Y, MAP_RESOLUTION);  
+
+    int eeprom_address = (x_index * MAP_RESOLUTION) + y_index;  
+
+    if (eeprom_address > 1023)
+    {
+        Serial.println(F("Error: EEPROM Address greater than 1023"));
+    }
+    else
+    {
+        char value = (char)EEPROM.read(eeprom_address);
+        if (value ==MAP_DEFAULT_FEATURE||value ==MAP_EXPLORED_FEATURE){
+          EEPROM.update(eeprom_address, feature);}
+    }
+    
+
+}
+
+
+#endif
